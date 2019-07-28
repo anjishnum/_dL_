62f77298db4ea12fe138fd9feccc24fba534e8bb
Anjishnu Mahanta <35238072+anjishnum@users.noreply.github.com>
2019-07-28 17:23:19 +0530

    Update keras_mnist.py

--- keras_mnist.py
+++ keras_mnist.py
@@ -18,3 +18,5 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Dense(512, activation=tf.nn.relu),
   tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics = ['accuracy'])
+model.fit(training_images, training_labels, epochs=5, callbacks=[callbacks])
