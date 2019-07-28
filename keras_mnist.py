1a84f213f56d5bebcd11bed9e1a7c531ccbd640a
Anjishnu Mahanta <35238072+anjishnum@users.noreply.github.com>
2019-07-28 17:22:19 +0530

    Update keras_mnist.py

--- keras_mnist.py
+++ keras_mnist.py
@@ -1,4 +1,9 @@
 import tensorflow as tf
 print(tf.__version__)
 
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('loss')<0.1):
+      print("\nReached 90% accuracy so cancelling training!")
+      self.model.stop_training = True
 
