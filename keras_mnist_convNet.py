05b4b55df4b56e306b66433bdeb5229a44986a92
Anjishnu Mahanta <35238072+anjishnum@users.noreply.github.com>
2019-07-29 01:21:23 +0530

    Update and rename keras_mnist_cnn.py to keras_mnist_convNet.py

--- keras_mnist_convNet.py
+++ keras_mnist_convNet.py
@@ -1,29 +1,31 @@
 import tensorflow as tf
-import keras
+#import keras
 import matplotlib.pyplot as plt
 
-class myCallbacks(keras.callbacks.Callback):
+class myCallback(tf.keras.callbacks.Callback):
   def on_epoch_end(self,epoch,logs={}):
     if(logs.get('acc')>0.95):
       print('\nStopping operation! Exceeded 95% accuracy!')
       self.model.stop_training = True
 
-callbacks = myCallbacks()
+callbacks = myCallback()
 
-mnist = keras.datasets.mnist
+mnist = tf.keras.datasets.fashion_mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+x_train = x_train.reshape(60000, 28, 28, 1)
 x_train = x_train/255.0
+x_test = x_test.reshape(10000, 28, 28, 1)
 x_test = x_test/255.0
 
-model = keras.models.Sequential([
-  keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(28,28,1)),
-  keras.layers.MaxPooling2D(2,2),
-  keras.layers.Conv2D(64, (3,3), activation='relu'),
-  keras.layers.MaxPooling2D(2,2),
-  keras.layers.Flatten(),
-  keras.layers.Dense(512, activation='relu'),
-  keras.layers.Dense(10, activation='softmax')
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(28,28,1)),
+  tf.keras.layers.MaxPooling2D(2,2),
+  tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+  tf.keras.layers.MaxPooling2D(2,2),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(512, activation='relu'),
+  tf.keras.layers.Dense(10, activation='softmax')
 ])
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
