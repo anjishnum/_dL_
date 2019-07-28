2ac2045b061edd194a8f87823db7c2b2ac0e0121
Anjishnu Mahanta <35238072+anjishnum@users.noreply.github.com>
2019-07-29 00:55:42 +0530

    Update keras_mnist_cnn.py

--- keras_mnist_cnn.py
+++ keras_mnist_cnn.py
@@ -27,4 +27,8 @@ model = keras.models.Sequential([
   keras.layers.Dense(10, activation='softmax')
 ])
 
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
+model.fit(x_train, y_train, epochs=5, callbacks=[callbacks])
+
+model.evaluate(x_test, y_test)
