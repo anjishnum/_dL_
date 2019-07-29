ddc7e18f93283c5f7f1b8b924d679c48477d4461
Anjishnu Mahanta <35238072+anjishnum@users.noreply.github.com>
2019-07-30 02:54:09 +0530

    Update _vision_cv2.py

--- _vision_cv2.py
+++ _vision_cv2.py
@@ -59,4 +59,23 @@ for x in range(1,size_x-1):
         convolution=255
       i_transformed_2[x, y] = convolution
       
+for x in range(1,size_x-1):
+  for y in range(1,size_y-1):
+      convolution = 0.0
+      convolution = convolution + (i[x - 1, y-1] * filter3[0][0])
+      convolution = convolution + (i[x, y-1] * filter3[0][1])
+      convolution = convolution + (i[x + 1, y-1] * filter3[0][2])
+      convolution = convolution + (i[x-1, y] * filter3[1][0])
+      convolution = convolution + (i[x, y] * filter3[1][1])
+      convolution = convolution + (i[x+1, y] * filter3[1][2])
+      convolution = convolution + (i[x-1, y+1] * filter3[2][0])
+      convolution = convolution + (i[x, y+1] * filter3[2][1])
+      convolution = convolution + (i[x+1, y+1] * filter3[2][2])
+      convolution = convolution * weight
+      if(convolution<0):
+        convolution=0
+      if(convolution>255):
+        convolution=255
+      i_transformed_3[x, y] = convolution       
+      
       
