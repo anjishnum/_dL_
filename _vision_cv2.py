9e7068b26d40017f9988b67389518c031733a962
Anjishnu Mahanta <35238072+anjishnum@users.noreply.github.com>
2019-07-30 02:57:14 +0530

    Update _vision_cv2.py

--- _vision_cv2.py
+++ _vision_cv2.py
@@ -126,3 +126,15 @@ for x in range(0, size_x, 2):
     pixels.append(i_transformed_2[x, y+1])
     pixels.append(i_transformed_2[x+1, y+1])
     newImage_2[int(x/2),int(y/2)] = max(pixels)    
+    
+new_x = int(size_x/2)
+new_y = int(size_y/2)
+newImage_3 = np.zeros((new_x, new_y))
+for x in range(0, size_x, 2):
+  for y in range(0, size_y, 2):
+    pixels = []
+    pixels.append(i_transformed_3[x, y])
+    pixels.append(i_transformed_3[x+1, y])
+    pixels.append(i_transformed_3[x, y+1])
+    pixels.append(i_transformed_3[x+1, y+1])
+    newImage_3[int(x/2),int(y/2)] = max(pixels)    
