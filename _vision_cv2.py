9bf3d71bc0baf0d59c8b29fee6cc2f3835fb666d
Anjishnu Mahanta <35238072+anjishnum@users.noreply.github.com>
2019-07-30 02:52:57 +0530

    Update _vision_cv2.py

--- _vision_cv2.py
+++ _vision_cv2.py
@@ -15,3 +15,9 @@ i_transformed_2 = np.copy(i)
 i_transformed_3 = np.copy(i)
 size_x = i_transformed_1.shape[0]
 size_y = i_transformed_1.shape[1]
+
+filter1 = [ [0, 1, 0], [1, -4, 1], [0, 1, 0]]
+filter2 = [ [-1, -2, -1], [0, 0, 0], [1, 2, 1]]
+filter3 = [ [-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]
+weight = 1
+
