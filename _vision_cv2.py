b709213452eca656df4f7bd37571313e805b7785
Anjishnu Mahanta <35238072+anjishnum@users.noreply.github.com>
2019-07-30 02:51:58 +0530

    Update _vision_cv2.py

--- _vision_cv2.py
+++ _vision_cv2.py
@@ -1,4 +1,12 @@
-  import cv2
-  import numpy as np
-  from scipy import misc
-  i = misc.ascent()
+import cv2
+import numpy as np
+from scipy import misc
+i = misc.ascent()
+
+import matplotlib.pyplot as plt
+#plt.grid(False)
+#plt.gray()
+#plt.axis('off')
+#plt.imshow(i)
+#plt.show()
+
